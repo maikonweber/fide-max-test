12ac78d390a6bea81a13935b1304eb69c3bb78ad
MaikonWeber <maikonweber@gmail.com>
2024-01-10 14:31:37 -0300

    Add form bootstrap

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Header from '@/app/component/header'
+import FormComponent from './component/header/questionForm'
 export default function Home() {
   return (
     <>
     <Header/>
+    <FormComponent/>
     </>
   )
 }
