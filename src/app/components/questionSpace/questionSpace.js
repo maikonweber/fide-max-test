6789eff5aeec3ae71b74f0e41c71c8dcef310f88
MaikonWeber <maikonweber@gmail.com>
2024-01-11 15:05:54 -0300

    add question

--- src/app/components/questionSpace/questionSpace.js
+++ src/app/components/questionSpace/questionSpace.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const RenderQuestion = ({ typeQuestion, answerValue, mandatory, content, itens }) => {
-  console.log(typeQuestion)
+  console.log(typeQuestion, "render")
   return (
     <div>
       <p>Type Question: {typeQuestion}</p>
