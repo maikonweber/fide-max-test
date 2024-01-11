ddf21244c99fc82848cfdbcea1bec2af347cede4
Maikon Weber <maikonweber@gmail.com>
2024-01-10 22:44:34 -0300

    Add component

--- src/app/component/questionForm/index.js
+++ src/app/component/questionForm/index.js
@@ -1,13 +1,11 @@
 const FormComponent = ({ answers }) => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-start h-screen">
             
             <h2 className="text-2xl font-bold mb-4">Pesquisa de Satisfação</h2>
             <div className="bg-white p-8 shadow-md rounded-md">
                 <form>
-                    <div className="mb-4">
-                      {/* Answers*/}  
-                    </div>
+                    
                     <button type="submit" className="bg-blue-500 text-white m-2 p-2 rounded-md hover:bg-blue-600">Enviar Fake Post</button>
                     <button type="submit" className="bg-blue-500 text-white m-2 p-2 rounded-md hover:bg-blue-600">Enviar Erro </button>
                     <button type="submit" className="bg-blue-500 text-white  m-2 p-2 rounded-md hover:bg-blue-600">Enviar Sucesso</button>
@@ -18,9 +16,10 @@ const FormComponent = ({ answers }) => {
 };
 
 export async function getServerSideProps() {
-    const response = await fetch('');
+    const response = await fetch('https://fdlmx-backgrounds.sfo3.digitaloceanspaces.com/front-test/survey.json ');
+    
     const answers = await response.json();
-
+    
     return {
         props: {
             answers
